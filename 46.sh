4ca3c3d490371dfc68d5b680c8e1998d3eb27f72
kun0769 <591619648@qq.com>
2020-01-18 23:31:39 +0800

    add 46.sh

---
+++ 46.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#program:
+#    getinterface.sh [-i interface | -I ip]
+#    当用户使用-i选项时，显示指定网卡的IP地址；当用户使用-I选项时，显示其指定ip所属的网卡
+#
+#history:
+#2020/01/18    kun    V1.0
+
+#把所有网卡写入临时文件
+ip add |awk -F ': ' '$1 ~/^[0-9]/ {print $2}' > /tmp/46_int.log
+
+#获得网卡对应IP地址
+get_ip(){
+    ip add |grep -w $1|grep 'inet'|awk -F '/' '{print $1}'|awk '{print $2}'
+}
+
+for int in `cat /tmp/46_int.log`;do
+    $int get_ip $int
+done > /tmp/46_int_ip.log
