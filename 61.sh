52655b4b4160ad46e1c8676b1de86ef5c7899910
kun0769 <591619648@qq.com>
2020-02-11 22:01:56 +0800

    add 61.sh

---
+++ 61.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#program:
+#    有两个文件a.txt和b.txt，需求是，把a.txt中有的但b.txt中没有的行发现来，并写入到c.txt，然后计算c.txt文件的行数。
+#
+#history:
+#2020/02/11    kun    V1.0
+
+cat a.txt |while read line
+do
+    if ! grep -q "$line" b.txt
+    then
+        echo $line
+    fi
+done >> c.txt
